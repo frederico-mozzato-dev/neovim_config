38dbc2144d88c38a703a94ca7297ebbe5f002be7
Frederico Mozzato <frederico.mozzato@rebase.com.br>
2024-05-16 11:12:52 -0300

    Add emmet language server LSP

--- init.lua
+++ init.lua
@@ -141,6 +141,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-rails',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -482,6 +483,7 @@ require('lazy').setup({
         ruby_lsp = {},
         rubocop = {},
         sorbet = {},
+        emmet_language_server = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
