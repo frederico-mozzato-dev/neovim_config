dad0efa8a75c7351708466b32224045a4c19a2fd
Frederico Mozzato <frederico.mozzato@rebase.com.br>
2024-05-29 15:51:15 -0300

    Add vim-endwise plugin

--- init.lua
+++ init.lua
@@ -145,6 +145,7 @@ require('lazy').setup({
   'tpope/vim-bundler',
   'tpope/vim-rake',
   'tpope/vim-fugitive',
+  'tpope/vim-endwise',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -730,11 +731,13 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
+    -- 'projekt0n/github-nvim-theme',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'github_dark_default'
       vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
